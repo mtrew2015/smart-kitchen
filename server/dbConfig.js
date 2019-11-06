ed293255bbb12fe9d0c07490683858f275e76f75
Michael Trew <mtrew2015@gmail.com>
2019-11-06 04:18:55 -0500

    working on tables/schema

---
+++ server/dbConfig.js
@@ -0,0 +1,6 @@
+const knex = require('knex');
+require('dotenv').config;
+const config = require('./knexfile');
+const dbEnv = process.env.NODE_ENV || 'development'
+
+module.exports = knex(config[dbEnv]);
---
+++ server/src/migrations/20191106041244_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+        users.string('email').notNullable();
+        users.string('username').notNullable();
+        users.string('password').notNullable();
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
