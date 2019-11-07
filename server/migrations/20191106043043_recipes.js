dfc146f660213dca8ad3846c8b6ea82e3ae70c3e
Michael Trew <mtrew2015@gmail.com>
2019-11-07 00:37:22 -0500

    modified schema/re-seeded

--- server/migrations/20191106043043_recipes.js
+++ server/migrations/20191106043043_recipes.js
@@ -1,10 +1,13 @@
 exports.up = function(knex) {
 	return knex.schema.createTable('recipes', (recipe) => {
-		recipe.increments('recipeId');
+        recipe.increments('recipeId');
+        recipe.string('title').notNullable();
         recipe.integer('author').unsigned().notNullable();
-        recipe.text('steps').notNullable()
-
-		recipe.foreign('author').references('userId').inTable('users');
+        recipe.text('steps').notNullable();
+        recipe.text('comments');
+        recipe.float('rating')
+        recipe.foreign('author').references('userId').inTable('users');
+        
 	});
 };
 
--- server/seeds/recipes.js
+++ server/seeds/recipes.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('recipes').insert([
-        {recipeId: 1, author: 1, steps:'Bake Cookies @ 400 for 50 mins, Remove From Oven, Enjoy'},
-        {recipeId: 2, author: 2, steps:'Bake Cookies @ 400 for 50 mins, Remove From Oven, Enjoy'},
-        {recipeId: 3, author: 2, steps:'Bake Cookies @ 400 for 50 mins, Remove From Oven, Enjoy'},
+        {recipeId: 1, title: "Cookies", author: 1, steps:'Bake Cookies @ 400 for 50 mins, Remove From Oven, Enjoy'},
+        {recipeId: 2, title: "Pork Chops", author: 2, steps:'Bake Pork Chops @ 400 for 50 mins, Remove From Oven, Enjoy'},
+        {recipeId: 3, title: "Muffins", author: 2, steps:'Bake Muffins @ 400 for 50 mins, Remove From Oven, Enjoy', comments: "Great Recipe, I can't wait to try this"},
       ]);
     });
 };
