15532b3674f7aa9fe896d282f957629de50f92a0
Michael Trew <mtrew2015@gmail.com>
2019-11-07 01:56:29 -0500

    add auth routes

---
+++ server/routers/recipeRouter.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../helpers/recipeHelpers');
+
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	db.getRecipeById(id).then((post) => res.json({ data: post })).catch((err) => res.json({ message: err }));
+});
+
+router.get('/recipes/:author', (req, res) => {
+	const { author } = req.params;
+	db.getRecipesByUser(author).then((recipes) => res.json({ data: recipes })).catch((err) => res.json({ message: err }));
+});
+
+router.post('/addrecipe', (req, res) => {
+	const recipe = req.body;
+	db
+		.insertRecipe(recipe)
+		.then((id) => res.json({ message: `Your post was created with an ID of ${id}` }))
+		.catch((err) => res.send(err));
+});
+
+router.put('/update/:id', (req, res) => {
+	const { id } = req.params;
+	const recipe = req.body;
+	db
+		.updateRecipe(id, recipe)
+		.then((id) => res.json({ message: `your recipe update of ${id} update was successful` }))
+		.catch((err) => res.send(err));
+});
+
+router.delete('delete/:id', (req, res) => {
+	const { id } = req.params;
+	db
+		.deletePost(id)
+		.then((count) => {
+			res.json({ message: `${count} number of records deleted` });
+		})
+		.catch((err) => res.send('Could not delete this post'));
+});
+
+module.exports = router;
