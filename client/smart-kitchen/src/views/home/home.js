8fa23b8171a4409a733a08ebdb57c7606c8a0b3c
Michael <40902786+mtrew2015@users.noreply.github.com>
2019-11-09 21:00:58 -0500

    setting up state

--- client/smart-kitchen/src/views/home/home.js
+++ client/smart-kitchen/src/views/home/home.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react'
-import './home.scss'
-import Nav from '../../components/nav/nav'
-import axios from 'axios'
-import RecipeCard from '../../components/recipeCard/recipeCard'
+import React, { Component } from 'react';
+import './home.scss';
+import Nav from '../../components/nav/nav';
+import axios from 'axios';
+import RecipeCard from '../../components/recipeCard/recipeCard';
 
- class Home extends Component {
-    render() {
-        return (
-            <div class="home-container">
-                <RecipeCard/>
-                <RecipeCard/>
-                <RecipeCard/>
-                
-            </div>
-        )
-    }
+class Home extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+	render() {
+		return (
+			<div class="home-container">
+				<RecipeCard />
+				<RecipeCard />
+				<RecipeCard />
+			</div>
+		);
+	}
 }
 
 export default Home;
