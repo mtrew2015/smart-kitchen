5531788285f93b94f141afbd62d35c93a50951c3
Michael <40902786+mtrew2015@users.noreply.github.com>
2019-11-09 21:17:01 -0500

    adds react-router

--- client/smart-kitchen/src/App.js
+++ client/smart-kitchen/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Nav from './components/nav/nav'
 import Home from './views/home/home';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Home/>
+       <Nav/>
+      <Route path exact = '/' component = {Home}/>
+     
     </div>
   );
 }
